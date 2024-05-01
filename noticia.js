21835a30577c67b0da648ba590afe81af23c3fd7
Fabio <fabiot92@gmail.com>
2024-05-01 02:07:56 -0300

    Update noticia.js

--- noticia.js
+++ noticia.js
@@ -1,21 +1,22 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function extrairDados() {
+async function extrairManchetes() {
     try {
         const response = await axios.get('https://g1.globo.com/globonews/');
         const html = response.data;
         const $ = cheerio.load(html);
 
-        // Aqui você pode selecionar os elementos do HTML e extrair os dados que deseja
-        const titulo = $('h1').text();
-        const paragrafos = $('p').map((i, el) => $(el).text()).get();
+        // Selecionar apenas os elementos <a> com a classe feed-post-link
+        const manchetes = $('a.feed-post-link').map((i, el) => $(el).text()).get();
 
-        console.log('Título:', titulo);
-        console.log('Parágrafos:', paragrafos);
+        console.log('Manchetes:');
+        manchetes.forEach((manchete, index) => {
+            console.log(`${index + 1}. ${manchete}`);
+        });
     } catch (error) {
         console.error('Ocorreu um erro:', error);
     }
 }
 
-extrairDados();
+extrairManchetes();
